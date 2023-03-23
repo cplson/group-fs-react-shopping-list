91239ac3c46359adcaaba607e17d7d585f9e0203
Lily LaCosse <lilylacosse@gmail.com>
2023-03-23 13:43:38 -0500

    wrote and tested get route

--- server/routes/list.route.js
+++ server/routes/list.route.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool.js');
+const pool = require("../modules/pool.js");
 
-router.get('/', (req, res) => {
-    console.log('in server GET');
-})
+router.get("/", (req, res) => {
+  console.log("in server GET");
+  const sqlText = "SELECT * FROM shopping_list ORDER BY NAME, name DESC;";
+
+  pool
+    .query(sqlText)
+    .then((result) => {
+      console.log(`Server GET response`, result);
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error making database query ${sqlText}`, error);
+      res.sendStatus(500); // Good server always responds
+    });
+});
 
 module.exports = router;
